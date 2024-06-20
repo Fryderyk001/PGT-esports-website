ad0b1dba3674ad280578a35dac9a569dc711fe17
Fryderyk001 <sochaplaygame@gmail.com>
2024-06-20 03:24:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     tlsAllowInvalidCertificates: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
 });
 
 app.use(session({
