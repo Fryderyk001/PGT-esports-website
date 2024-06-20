d6341bba7ebd44a449859781b2653e6b40ad8006
Fryderyk001 <sochaplaygame@gmail.com>
2024-06-20 02:43:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    tlsAllowInvalidCertificates: true
 });
 
 app.use(bodyParser.json());
