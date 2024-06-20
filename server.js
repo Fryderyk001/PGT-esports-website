b4d298cda95c1c4ddb8cacf66260ba9b790236da
Fryderyk001 <sochaplaygame@gmail.com>
2024-06-20 16:11:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/about.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
